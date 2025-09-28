9ab560cfaa167a8877bf316efd7c5889347443b5
CeciTactaca <cy.a.z18@gmail.com>
2025-09-28 18:43:20 -0300

    Se le agrego un script para el movimiento a Player

---
+++ Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+/// <summary>
+/// Permite el comportamiento del movimiento del jugador
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+    #region Atributos
+    /// <summary>
+    /// Fuerza utilizada para aplicar movimiento 
+    /// </summary>
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
