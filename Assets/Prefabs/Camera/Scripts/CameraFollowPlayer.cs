e9352d735ddc808ebdb4b319cfd710c1e8e82144
CeciTactaca <cy.a.z18@gmail.com>
2025-10-05 23:47:49 -0300

    Se corrigio un error en el script

---
+++ Assets/Prefabs/Camera/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    private Vector3 offSet;
+    private PlayerMovement playerMovement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offSet = new Vector3(0, 1, -5);
+        playerMovement = FindFirstObjectByType<PlayerMovement>();
+    }
+
+    void LateUpdate()
+    {
+        gameObject.transform.position = playerMovement.transform.position + offSet;
+    }
+}
